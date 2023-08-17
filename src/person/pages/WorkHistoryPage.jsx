c869267642d5de2bbb700cda61751d7cb9364110
Guillermo Villalobos <j_guillermo_villa@outlook.com>
2023-08-16 21:58:22 -0600

    initial commit

---
+++ src/person/pages/WorkHistoryPage.jsx
@@ -0,0 +1,65 @@
+export const WorkHistoryPage = () => {
+  return (
+    <main className="px-3">
+      <div className="row mb-4 align-items-center">
+      <div className="col-md-12 overflow-auto mt-2">
+        <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" className="scrollspy-example" tabIndex="0">
+          <h4 id="simple-list-item-1">Analyst Developer</h4>
+          <span className="badge text-bg-primary">Secretaria de Desarrollo Rural</span>
+          <br />
+          <span className="badge text-bg-secondary">October 2022 - Actually</span>
+          <p>Full Stack Analyst Developer focused on the administrative 
+              requirements of the Department of Rural Development. 
+              Responsible for the analysis and design of IT solutions with a 
+              focus on software development.</p>
+          <b>Techs:</b>
+          <div className="d-flex justify-content-center align-items-center mb-2 flex-wrap">
+            <span className="badge text-bg-info m-1">JavaScript</span>
+            <span className="badge text-bg-info m-1">C#</span>
+            <span className="badge text-bg-info m-1">CSS</span>
+            <span className="badge text-bg-info m-1">HTML</span>
+            <span className="badge text-bg-info m-1">jQuery</span>
+            <span className="badge text-bg-info m-1">ASP .Net</span>
+            <span className="badge text-bg-info m-1">SQL</span>
+          </div>
+          <h4 id="simple-list-item-2">Lead Software Development Engineer</h4>
+          <span className="badge text-bg-primary">Westmount Solutions</span>
+          <br />
+          <span className="badge text-bg-secondary">February 2020 - February 2022</span>
+          <p>Started as a developer in the development of e-commerce platforms, I became development team leader after 8 months.</p>
+          <b>Techs:</b>
+          <div className="d-flex justify-content-center align-items-center mb-2 flex-wrap">
+            <span className="badge text-bg-info m-1">JavaScript</span>
+            <span className="badge text-bg-info m-1">Node.js</span>
+            <span className="badge text-bg-info m-1">PHP</span>
+            <span className="badge text-bg-info m-1">CodeIgniter</span>
+            <span className="badge text-bg-info m-1">Shopify</span>
+            <span className="badge text-bg-info m-1">Liquid</span>
+            <span className="badge text-bg-info m-1">CSS</span>
+            <span className="badge text-bg-info m-1">HTML</span>
+            <span className="badge text-bg-info m-1">jQuery</span>
+            <span className="badge text-bg-info m-1">WordPress</span>
+          </div>
+          <h4 id="simple-list-item-3">Analyst Developer</h4>
+          <span className="badge text-bg-primary">UTCH</span>
+          <br />
+          <span className="badge text-bg-secondary">February 2018 - February 2020</span>
+          <p>Responsible for the Finance, Human Resources, and study plans, implementing new developments for the Finance and Human Resources modules, and restructuring the study plan module
+            </p>
+          <b>Techs:</b>
+          <div className="d-flex justify-content-center align-items-center mb-2 flex-wrap">
+            <span className="badge text-bg-info m-1">JavaScript</span>
+            <span className="badge text-bg-info m-1">PHP</span>
+            <span className="badge text-bg-info m-1">CSS</span>
+            <span className="badge text-bg-info m-1">HTML</span>
+            <span className="badge text-bg-info m-1">jQuery</span>
+            <span className="badge text-bg-info m-1">Java</span>
+            <span className="badge text-bg-info m-1">Hibernate</span>
+          </div>
+          
+        </div>
+      </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/ui/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-auto text-white-50">
+        <p>
+            By <a href="#" className="text-white">Guillermo Villalobos</a> 2024
+        </p>
+    </footer>
+  )
+}
